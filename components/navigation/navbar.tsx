1b5797aa9057e0848a691613bbd1d4e1e2ca31cf
Uli <ulisesvetere07@gmail.com>
2026-05-22 19:34:30 -0300

    Actualizacion version NextJS

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -7,17 +7,14 @@ export default function Navbar() {
     <nav className="border-b bg-white sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
-          {/* Logo container */}
           <div className="flex-1 flex items-center">
             <Logo />
           </div>
 
-          {/* Desktop Navigation - Centered */}
           <div className="hidden md:flex flex-none items-center justify-center">
             <NavLinks />
           </div>
 
-          {/* Mobile Menu / Right Spacer */}
           <div className="flex-1 flex justify-end items-center">
             <MenuToggle />
           </div>
